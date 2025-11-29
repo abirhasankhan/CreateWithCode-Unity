41e9c220a3efc58936ac4cd520f96656f105cf55
Abir Khan <ak99.abirkhan@gmail.com>
2025-11-30 01:10:26 +0600

    When the player presses the up/down arrows, the vehicle will move forward and backward When the player presses the left/right arrows, the vehicle turns

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
-    
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float VerticalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+
         // move the vehicle forward
         // transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * VerticalInput);
+        // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        
+        // turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
